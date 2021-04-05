fd0c967775597336d4c5393bd0be8b7e3b0c80ed
Tyler Perkins <tperki38@bruinmail.slcc.edu>
2021-04-05 13:44:43 -0700

    Add project files.

---
+++ A04_Snake/MainBoard.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace A04_Snake
+{
+    class MainBoard
+    {
+        public int widthOfBoard { get; set; }
+        public int heightOfBoard { get; set; }
+        public static int gameSpeed { get; set; }
+        
+
+        public MainBoard (int heightOfBoard, int widthOfBoard)
+        {
+            this.widthOfBoard = widthOfBoard;
+            this.heightOfBoard = heightOfBoard;
+        }
+
+        public void startGame()
+        {
+            Console.SetCursorPosition(50, 5);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Welcome To SNAKE!");
+            Console.SetCursorPosition(40, 6);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Controls: UP/DOWN/LEFT/RIGHT Arrow Keys");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(20, 7);
+            Console.WriteLine("Directions: Avoid biting yourself, hitting the walls, and hitting the obstacles");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(25, 8);
+            Console.WriteLine("Press <ENTER> Once You Understand The Game Rules And How To Play Snake!");
+            Console.ReadLine();
+            Console.Clear();
+            secondScreen();
+        }
+        public void secondScreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(40, 5);
+            Console.WriteLine("Choose the difficulty of the game!");
+            Console.SetCursorPosition(52, 6);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1: Easy");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(52, 7);
+            Console.WriteLine("2: Normal");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(52, 8);
+            Console.WriteLine("3: Hard");
+            Console.SetCursorPosition(45, 9);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Your Difficulty Choice: ");
+            int userDifficulty = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if (userDifficulty == 1)
+            {
+                gameSpeed = 50;
+                Console.SetCursorPosition(28, 0);
+                Console.WriteLine("Snake : EASY");
+                drawGameBorder();
+               
+            }
+            if (userDifficulty == 2)
+            {
+                gameSpeed = 30;
+                Console.SetCursorPosition(28, 0);
+                Console.WriteLine("Snake : NORMAL");
+                drawGameBorder();
+            }
+            if (userDifficulty == 3)
+            {
+                gameSpeed = 5;
+                Console.SetCursorPosition(28, 0);
+                Console.WriteLine("Snake : HARD");
+                drawGameBorder();
+            }
+            
+        }
+        public void drawGameBorder()
+        {         
+            Console.SetCursorPosition(5, 2);
+            for (int i = 0; i < heightOfBoard; i++)
+            {                              
+                for (int h = 0; h < widthOfBoard; h++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+                Console.SetCursorPosition(5, i+2);               
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    } 
+}
+
+
+
+
+
+
+
---
+++ A04_Snake/Snake_Body_Movements.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+using System.Resources;
+
+
+namespace A04_Snake
+{
+    struct mrSnakePosition
+    {
+        public int row;
+        public int col;
+       
+
+        public mrSnakePosition(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+    }
+    class Snake_Body_Movements
+    {
+ 
+        public static void Main(string[] args)
+        {
+            int right = 0;
+            int left = 1;
+            int down = 2;
+            int up = 3;
+            var timer = new Stopwatch();
+            bool gameLive = true;
+
+
+            MainBoard game = new(20, 60);
+            game.startGame();
+            int snakeSpeed = MainBoard.gameSpeed;
+            timer.Start();
+
+            Console.CursorVisible = false;
+
+
+            mrSnakePosition[] mrSnakeDirection = new mrSnakePosition[]
+            {
+                //right
+                new mrSnakePosition(0,1),
+                //left
+                new mrSnakePosition(0,-1),
+                //down
+                new mrSnakePosition(1,0),
+                //right
+                new mrSnakePosition(-1, 0)
+            };
+
+            //starting direction
+            int startDirection = right;
+
+            List<mrSnakePosition> inhibit = new List<mrSnakePosition>()
+            {
+                
+                new mrSnakePosition(11, 15),
+                new mrSnakePosition(6, 22),
+                new mrSnakePosition(18, 17),
+                new mrSnakePosition(5, 45),
+                new mrSnakePosition(14, 31),
+                new mrSnakePosition(17, 51)
+            };
+            foreach (mrSnakePosition inhibiting in inhibit)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.SetCursorPosition(inhibiting.col, inhibiting.row);
+                Console.Write("||");
+            }
+            Queue<mrSnakePosition> mrSnakeParts = new Queue<mrSnakePosition>();
+            
+            for (int i = 0; i <= 10; i++)
+            {
+                mrSnakeParts.Enqueue(new mrSnakePosition(0+10, i+25));
+            }
+                foreach(mrSnakePosition snakePos in mrSnakeParts)
+            {
+                Console.SetCursorPosition(snakePos.col, snakePos.row);
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write("  ");
+           
+            }
+
+            while (gameLive)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo arrowInputs = Console.ReadKey();
+                    if(arrowInputs.Key == ConsoleKey.UpArrow)
+                    {
+                        if (startDirection != down) startDirection = up;
+                    }
+                    if (arrowInputs.Key == ConsoleKey.DownArrow)
+                    {
+                        if (startDirection != up) startDirection = down;
+                    }
+                    if (arrowInputs.Key == ConsoleKey.LeftArrow)
+                    {
+                        if (startDirection != right) startDirection = left;
+                    }
+                    if (arrowInputs.Key == ConsoleKey.RightArrow)
+                    {
+                        if (startDirection != left) startDirection = right;
+                    }
+                }
+                TimeSpan gameTime = timer.Elapsed;
+                mrSnakePosition mrSnakeHead = mrSnakeParts.Last();
+                mrSnakePosition movingNewDirection = mrSnakeDirection[startDirection];
+                mrSnakePosition newMrSnakeHead = new(mrSnakeHead.row + movingNewDirection.row, mrSnakeHead.col + movingNewDirection.col);
+                if ((newMrSnakeHead.col < 6) || (newMrSnakeHead.col > 63))
+                {
+                    Console.Beep();
+                    gameLive = false;
+                    gameOver(gameTime);
+                    timer.Stop();
+                    return;
+                }
+                if ((newMrSnakeHead.row < 3) || (newMrSnakeHead.row > 19))
+                {
+                    Console.Beep();
+                    gameLive = false;
+                    gameOver(gameTime);
+                    timer.Stop();
+                    return;
+                }
+
+                if(mrSnakeParts.Contains(newMrSnakeHead)) 
+                {
+                    Console.Beep();
+                    gameOver(gameTime);
+                    timer.Stop();
+                    return;
+                }
+                if (inhibit.Contains(newMrSnakeHead))
+                {
+                    Console.Beep();
+                    gameOver(gameTime);
+                    timer.Stop();
+                    return;
+                }
+
+                mrSnakeParts.Enqueue(newMrSnakeHead);
+                Console.SetCursorPosition(newMrSnakeHead.col, newMrSnakeHead.row);
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(" ");
+
+                mrSnakePosition lastPartOfMrSnake = mrSnakeParts.Dequeue();
+                Console.SetCursorPosition(lastPartOfMrSnake.col, lastPartOfMrSnake.row);
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.Write(" ");
+
+                Thread.Sleep(snakeSpeed);
+              
+                Console.SetCursorPosition(newMrSnakeHead.col, newMrSnakeHead.row);
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write(" ");
+
+                Thread.Sleep(snakeSpeed + 5);
+               
+            }
+
+        }
+        public static void gameOver(TimeSpan gameTime)
+        {
+            Console.SetCursorPosition(20, 1);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Game Over! ");
+            Console.Write("Time Alive: " + gameTime.ToString(@"m\:ss\.fff"));
+            Console.SetCursorPosition(0, 20);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Black;
+            return;
+        }
+    }   
+}
+
+
+
+
